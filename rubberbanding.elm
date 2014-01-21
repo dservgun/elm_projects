ce957d5d693abbace47ea15e4e90b0be2b1c544c
Dinkar Ganti <dinkar_ganti@hotmail.com>
2014-01-21 09:36:59 -0500

    Minor fix. There is still the issue of the mouse up

--- rubberbanding.elm
+++ rubberbanding.elm
@@ -3,36 +3,50 @@ import Json
 import Mouse
 import Window
 
+{--
+Rubber banding example. 
+For the example to work correctly, we need another signal: clearing or
+getting out of the draw mode.
+The basic tutorial talks about the following structure of each elm application:
+Model, Update and Display.
 
+--}
 
-isEven n = n `mod` 2 == 0
-clearGrey = rgba 111 111 111 0.6
+-- UPDATE
 mouseDown = keepWhen Mouse.isDown (0,0) Mouse.position
-mouseDownLocations = foldp (::) [] (mouseDown)
 mouseDownOrigin = foldp (\n aList -> 
                             case aList of
                              [] -> n :: []
                              _ -> aList) [] mouseDown
 
+
+
+  
+main = lift3 scene mouseDownOrigin Mouse.position Window.height
+
+
+-- DISPLAY    
+scene origin (w,h) windowHeight=
+        let 
+            (dx, dy) = decode origin (w,h) windowHeight
+            c = collage w h
+                [ rect (toFloat w) (toFloat h) |> filled blue |> move (dx, dy)
+                ]
+        in layers [c, message]
+-- UTILITIES
+-- For debugging.
 textDraw origin destination = 
   case origin of
   [x] -> (x, destination)
   _ -> (destination, destination)
 
-
-main = lift2 scene2 mouseDownOrigin Mouse.position
-
-decode origin  (r1, r2)=
+decode origin  (r1, r2) windowHeight=
     case origin of
-    [(x,y)] -> (toFloat x, toFloat y)
+    [(x,y)] -> ((toFloat x), toFloat y  - (toFloat windowHeight / 2))
     _ -> (toFloat r1, toFloat r2)
-    
-scene2 origin (w,h) =
-        let 
-            (dx, dy) = decode origin (w,h)
-        in  
-            collage w h
-           [ rect (toFloat w) (toFloat h) |> filled blue |> move (dx, dy)
-           ]
-
-
+           
+isEven n = n `mod` 2 == 0
+clearGrey = rgba 111 111 111 0.6
+message = [markdown|
+A rubber banding example
+|]
