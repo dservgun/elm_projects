33c8bb90c58a5c3caa39bc7e247aebb331ed4b3a
Dinkar Ganti <dinkar_ganti@hotmail.com>
2014-01-25 14:34:07 -0500

    Minor update

--- drawingboard.elm
+++ drawingboard.elm
@@ -1,9 +1,9 @@
 {-- Drawing board  --}
 import Mouse
 import Window
-
-initial = {down = False, start_x = -1, start_y = -1, cur_x = -1, cur_y = -1, 
-          shapes = []}
+shape = {x = -1, y = -1, w = -1, h = -1}
+shapes = []
+initial = {down = False, start_x = -1, start_y = -1, cur_x = -1, cur_y = -1}
 
 updateState (mouseDown, (xpos,ypos)) state =
   {state |  down <- mouseDown,
@@ -13,24 +13,25 @@ updateState (mouseDown, (xpos,ypos)) state =
            , start_y <- if | mouseDown && (state.down == False) -> ypos
                          | mouseDown && state.down -> state.start_y
                          | mouseDown == False -> -1
-           , cur_x <- if | mouseDown == False -> -1 | otherwise -> xpos
-           , cur_y <- if | mouseDown == False -> -1 | otherwise -> ypos
+           , cur_x <-  if | mouseDown == False -> -1 | otherwise -> xpos
+           , cur_y <-  if | mouseDown == False -> -1 | otherwise -> ypos           
    }
 
-drawRect (w, h) aState = 
+updateShapes aState = aState :: shapes   
+drawRect aState = 
                 let 
-                      cw = abs <| aState.cur_x - aState.start_x
-                      ch = abs <| aState.cur_y - aState.start_y
-                      s = rect (toFloat (aState.cur_x - aState.start_x)) 
-                        (toFloat (aState.cur_y - aState.start_y))
+                      cw = abs <| aState.start_x - aState.cur_x
+                      ch = abs <| aState.start_y - aState.cur_y
+                      s = rect (toFloat cw)
+                          (toFloat ch)
                         |> filled blue
-                        |> move (toFloat aState.start_x, toFloat <| -1 * aState.start_y)
-                      c = collage cw ch [s]
+                        |> move (toFloat <| (0 + aState.start_x), toFloat <| (0 - aState.start_y))
+                      c = collage (cw) (ch) [s]                      
                     in 
                       layers [c, message, asText aState]
                       
 --main = asText <~ foldp updateState initial (lift2 (,) Mouse.isDown Mouse.position)                                           
-main = drawRect <~ Window.dimensions ~ foldp updateState initial (lift2 (,) Mouse.isDown Mouse.position)
+main = drawRect <~ foldp updateState initial (lift2 (,) Mouse.isDown Mouse.position)
 
 message = [markdown|
 A rubber banding example: drag the mouse anywhere on the screen.
