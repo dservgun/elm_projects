2fe472c7842882c986752b3a854cbfecbfe66074
Dinkar Ganti <dinkar_ganti@hotmail.com>
2014-01-26 13:34:37 -0500

    Prettify the logic

--- drawingboard.elm
+++ drawingboard.elm
@@ -1,24 +1,30 @@
 {-- Drawing board  --}
+{-- The objective of this board is to present a user with a list of selections 
+that can be rubberbandable.
+--}
 import Mouse
 import Window
 shape = {x = -1, y = -1, w = -1, h = -1}
 shapes = []
-initial = {down = False, start_x = -1, start_y = -1, cur_x = -1, cur_y = -1}
-
-updateState (mouseDown, (xpos,ypos)) state =
-  {state |  down <- mouseDown,
-            start_x  <- if | mouseDown == True && (state.down == False) -> xpos
-                         | mouseDown == False -> -1
-                         | mouseDown && state.down -> state.start_x
-           , start_y <- if | mouseDown && (state.down == False) -> ypos
-                         | mouseDown && state.down -> state.start_y
-                         | mouseDown == False -> -1
-           , cur_x <-  if | mouseDown == False -> -1 | otherwise -> xpos
-           , cur_y <-  if | mouseDown == False -> -1 | otherwise -> ypos           
-   }
-
-updateShapes aState = aState :: shapes   
-drawRect aState = 
+data DragState = Started | Released
+dragState = Released
+drawing_bounds = {start_x = -1, start_y = -1, cur_x = -1, cur_y = -1}
+updateDragState isDown aState = if | isDown == True -> case aState of
+                                            Released -> Started
+                                            Started -> Started
+                                  | not isDown -> case aState of
+                                            Started -> Released
+                                            Released -> Released
+updateDrawingBounds (state, (x,y)) bounds = case state of
+                                          Started -> {bounds | start_x <- if  | bounds.start_x == -1 -> x 
+                                                                              | otherwise -> bounds.start_x
+                                                               ,start_y <- if | bounds.start_y == -1 -> y
+                                                                             | otherwise -> bounds.start_y
+                                                               , cur_x <- x
+                                                               , cur_y <- y}
+                                                               
+                                          Released -> {start_x = -1, start_y = -1, cur_x = -1, cur_y = -1}
+drawRect (aState,_) = 
                 let 
                       cw = abs <| aState.start_x - aState.cur_x
                       ch = abs <| aState.start_y - aState.cur_y
@@ -31,7 +37,11 @@ drawRect aState =
                       layers [c, message, asText aState]
                       
 --main = asText <~ foldp updateState initial (lift2 (,) Mouse.isDown Mouse.position)                                           
-main = drawRect <~ foldp updateState initial (lift2 (,) Mouse.isDown Mouse.position)
+main = let 
+            currentDragState = foldp updateDragState dragState Mouse.isDown
+            bounds = foldp updateDrawingBounds drawing_bounds(lift2 (,) currentDragState Mouse.position)
+       in            
+            drawRect <~ (lift2 (,) bounds currentDragState)
 
 message = [markdown|
 A rubber banding example: drag the mouse anywhere on the screen.
