79953fce416570a7303f565e00380c4f52c9da9e
Dinkar Ganti <dinkar_ganti@hotmail.com>
2014-01-28 00:10:20 -0500

    The coordinate system needs fixing

--- drawingboard.elm
+++ drawingboard.elm
@@ -8,8 +8,6 @@ import Window
 shape = {x = -1, y = -1, w = -1, h = -1}
 shapes = []
 data DragState = Started | Released
-collage_width = 500
-collage_height = 500                  
 
 dragState = Released
 drawing_bounds = {state = Released, start_x = -1, start_y = -1, cur_x = -1, cur_y = -1}
@@ -20,49 +18,53 @@ isValid s = s.start_x > 0 || s.start_y > 0 || s.cur_x > 0 || s.cur_y > 0
 updateShapes (s, aState) aList = 
     case aState of
     Released -> case aList of 
-                    [] -> if | isValid s -> s :: aList
-                             | otherwise -> aList
+                    [] -> if | isValid s -> [s]
+                             | otherwise -> []
                     h::t -> if 
-                            | not (areEqual h s) -> s :: aList
+                            | (not (areEqual h s)) && (isValid s) -> s :: aList
                             | otherwise -> aList
     Started -> aList
                                
                                           
                                              
-updateDragState isDown aState = if | isDown == True -> case aState of
-                                            Released -> Started
-                                            Started -> Started
-                                  | not isDown -> case aState of
-                                            Started -> Released
-                                            Released -> Released
+updateDragState isDown aState = if | isDown == True -> 
+                                     case aState of
+                                        Released -> Started
+                                        Started -> Started
+                                   | not isDown -> 
+                                     case aState of
+                                        Started -> Released
+                                        Released -> Released
+                                        
 updateDrawingBounds (iState, (x,y)) bounds = case iState of
-                                          Started -> {bounds | start_x <- case bounds.state of
-                                                                    Released -> x
-                                                                    _ -> bounds.start_x
-                                                               ,start_y <- case bounds.state of
-                                                                    Released -> x
-                                                                    _ -> bounds.start_y
-                                                               , cur_x <- x
-                                                               , cur_y <- y
-                                                               , state <- iState}                                                              
-                                          Released -> {bounds |
-                                                        start_x <- case bounds.state of
-                                                                Released -> -1
-                                                                _ -> bounds.start_x
-                                                        , start_y <- case bounds.state of
-                                                                Released -> -1
-                                                                _ -> bounds.start_y
-                                                        , cur_x <- case bounds.state of
-                                                                Released -> -1
-                                                                _ -> bounds.cur_x
-                                                        , cur_y <- case bounds.state of
-                                                                Released -> -1
-                                                                _ -> bounds.cur_y
-                                                        ,state  <- iState}
+                              Started -> {bounds | 
+                                            start_x <- case bounds.state of
+                                                        Released -> x
+                                                        _ -> bounds.start_x
+                                            ,start_y <- case bounds.state of
+                                                        Released -> x
+                                                        _ -> bounds.start_y
+                                            , cur_x <- x
+                                            , cur_y <- y
+                                            , state <- iState}                                                              
+                              Released -> {bounds |
+                                            start_x <- case bounds.state of
+                                                    Released -> -1
+                                                    _ -> bounds.start_x
+                                            , start_y <- case bounds.state of
+                                                    Released -> -1
+                                                    _ -> bounds.start_y
+                                            , cur_x <- case bounds.state of
+                                                    Released -> -1
+                                                    _ -> bounds.cur_x
+                                            , cur_y <- case bounds.state of
+                                                    Released -> -1
+                                                    _ -> bounds.cur_y
+                                            ,state  <- iState}
 
 drawBoundingRect aShape = 
                       let
-                        (dx, dy) = decode aShape
+                        (dx, dy) = translate aShape
                         cw = abs <| aShape.cur_x - aShape.start_y
                         ch = abs <| aShape.cur_y - aShape.start_y
                         s = rect (toFloat cw)
@@ -72,22 +74,23 @@ drawBoundingRect aShape =
                       in
                         s
 
-decode aShape = (aShape.cur_x, aShape.cur_y)
+translate aShape = (aShape.start_x, aShape.start_y)
 
-drawRect (aShape,aList) = 
+drawRect (width, height) (aShape,aList) = 
                 let 
                       s = drawBoundingRect aShape
-                      h = drawHistory aList
+                      hist = drawHistory aList
                     in 
-                      layers [collage collage_width collage_height ([s]++h), message, asText aShape]
+                      layers [collage width height ([s] ++ hist), message, asText aList]
 drawHistory aList = map drawBoundingRect aList
-scaleMousePosition (x,y) = (truncate <| (100 * (toFloat x)/collage_width), truncate <| (100*(toFloat y)/collage_height))
+scaleMousePosition (width, height) (x,y) = (x, y)
+
 main = let 
             currentDragState = foldp updateDragState dragState Mouse.isDown
-            bounds = foldp updateDrawingBounds drawing_bounds(lift2 (,) currentDragState (lift scaleMousePosition Mouse.position))
+            bounds = foldp updateDrawingBounds drawing_bounds(lift2 (,) currentDragState (lift2 scaleMousePosition Window.dimensions Mouse.position))
             shapeList = foldp updateShapes shapes (lift2 (,) bounds currentDragState)            
        in            
-            drawRect <~ (lift2 (,) bounds shapeList)
+            lift2 drawRect Window.dimensions (lift2 (,) bounds shapeList)
             
 
 message = [markdown|
