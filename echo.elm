c48f80f8e42cc518bae610f2af02441c8d2647b3
Dinkar Ganti <dinkar_ganti@hotmail.com>
2014-02-17 09:18:43 -0500

    Minor update.

--- echo.elm
+++ echo.elm
@@ -1,12 +1,27 @@
 import Char
-import String
 import Maybe
 import Http
 import Graphics.Input as Input
 import WebSocket
 
 defaultUrl = "ws://localhost:8082"
+
+type UIState = {requests : [String], 
+                responses : [String]}
+initState : UIState
+initState = {requests = [],
+             responses = []}
+
+incoming : Signal String
+incoming = WebSocket.connect defaultUrl outgoing
+
+outgoing : Signal String
+outgoing = content
+
 (field, content) = Input.field "Echo"
 drawString aString = plainText <| "" ++ aString
 outgoingMessages = WebSocket.connect defaultUrl content
-main =  lift2 above field (lift drawString outgoingMessages)
+
+     
+
+main =  lift2 above field (lift drawString incoming)
